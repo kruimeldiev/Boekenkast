c0f7385e802aa8259ee5627cb24ab6a0253b0938
Casper Daris <daris@d-tt.nl>
2024-11-02 14:27:33 +0100

    initial commit

---
+++ boekenkast/src/main/java/nl/casperdaris/boekenkast/BoekenkastApiApplication.java
@@ -0,0 +1,13 @@
+package nl.casperdaris.boekenkast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoekenkastApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoekenkastApiApplication.class, args);
+	}
+
+}
---
+++ boekenkast/src/test/java/nl/casperdaris/boekenkast/BoekenkastApiApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.casperdaris.boekenkast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoekenkastApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
